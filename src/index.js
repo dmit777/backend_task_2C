b6b6281cb63a5439c0ff2722f927f52b400e9b5b
Dmitry Parsegov <dprsv@yandex.ru>
2016-11-09 01:10:58 +0300

    initial commit for task 2B

--- src/index.js
+++ src/index.js
@@ -16,6 +16,47 @@ app.get('/task2A', (req, res) => {
 });
 
 
+function nameContainsNumbers(fullName) {
+  if (/(\d|_|\/)/.test(fullName))
+    return true;
+  else {
+    return false;
+  }
+}
+
+function procName(firstName) {
+  return firstName.charAt(0).toUpperCase() + '.';
+}
+
+
+app.get('/task2B', (req, res) => {
+  if (req.query.fullname.length == 0)
+    return res.send('Invalid fullname');
+  if (nameContainsNumbers(req.query.fullname))
+    return res.send('Invalid fullname');
+  let iofArr = (req.query.fullname).split(' ');
+  switch(iofArr.length) {
+    case 1: {
+      res.send(iofArr[0]);
+      break;
+    }
+    case 2: {
+      const familyName = iofArr[1] + ' ' + procName(iofArr[0]);
+      res.send(familyName);
+      break;
+    }
+    case 3: {
+     const fio = iofArr[2] + ' ' + procName(iofArr[0])
+     + ' ' + procName(iofArr[1]);
+     res.send(fio);
+     break;
+    }
+    default: {
+     res.send('Invalid fullname');
+    }
+  }
+});
+
 
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
